94d34f115595f4df482f222444af550be5962cb5
Michael Parlee <shralpmeister@me.com>
2013-09-16 21:57:15 -0700

    More non-working iOS 7 changes.

---
+++ SDApplicationState.m
@@ -0,0 +1,17 @@
+//
+//  SDApplicationState.m
+//  ShralpTide2
+//
+//  Created by Michael Parlee on 9/2/13.
+//
+//
+
+#import "SDApplicationState.h"
+#import "SDFavoriteLocations.h"
+
+
+@implementation SDApplicationState
+
+@dynamic favoriteLocations;
+
+@end
---
+++ SDFavoriteLocation.m
@@ -0,0 +1,18 @@
+//
+//  SDFavoriteLocations.m
+//  ShralpTide2
+//
+//  Created by Michael Parlee on 9/2/13.
+//
+//
+
+#import "SDFavoriteLocations.h"
+#import "SDApplicationState.h"
+
+
+@implementation SDFavoriteLocations
+
+@dynamic locationName;
+@dynamic applicationState;
+
+@end
