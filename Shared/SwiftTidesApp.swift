4a6042329cc526c22938be5c91e3af5f11f3bd30
Michael Parlee <shralpmeister@mac.com>
2020-07-12 10:31:45 -0700

    Initial Commit

---
+++ Shared/SwiftTidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTidesApp.swift
+//  Shared
+//
+//  Created by Michael Parlee on 7/12/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
