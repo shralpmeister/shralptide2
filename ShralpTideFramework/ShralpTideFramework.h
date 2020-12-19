98b4e1c8978e29498f9c9256226c47355445b99d
Michael Parlee <shralpmeister@mac.com>
2020-12-18 16:37:23 -0800

    Initial Commit

---
+++ ShralpTideFramework/ShralpTideFramework.h
@@ -0,0 +1,18 @@
+//
+//  ShralpTideFramework.h
+//  ShralpTideFramework
+//
+//  Created by Michael Parlee on 12/18/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ShralpTideFramework.
+FOUNDATION_EXPORT double ShralpTideFrameworkVersionNumber;
+
+//! Project version string for ShralpTideFramework.
+FOUNDATION_EXPORT const unsigned char ShralpTideFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShralpTideFramework/PublicHeader.h>
+
+
